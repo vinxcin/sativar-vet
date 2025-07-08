9a431a738365cd36fa49bd060bd4c96b8de23c07
vinxcin <vinxciusrosa@gmail.com>
2025-07-08 02:00:24 -0300

    add bg static for small media

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,20 +1,12 @@
 import Hero from "./Hero";
+import MediaBackground from "./MediaBackground";
 import NavBar from "./NavBar";
 import StarsBackground from "./StarsBackground";
 
 export default function LandingPage() {
     return (
         <div className="relative w-full h-screen overflow-hidden">
-            <video
-                autoPlay
-                muted
-                loop
-                playsInline
-                className="fixed rotate-180 top-0 left-0 w-full h-full object-cover z-[-1]  translate-y-[-38vh] lg:translate-y-[-40vh]"
-            >
-                <source src="/videos/blackhole.webm" type="video/webm" />
-                Seu navegador não suporta vídeos em background.
-            </video>
+            <MediaBackground/>
 
             <div className="fixed top-0 left-0 w-full h-full bg-black/40 z-0" />
 
