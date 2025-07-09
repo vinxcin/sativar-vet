71b1a6bd6c69b96bdf293e826b328161faf6f99d
vinxcin <vinxciusrosa@gmail.com>
2025-07-08 21:18:53 -0300

    alter width bg black hole mobile

--- src/components/main/Hero/index.tsx
+++ src/components/main/Hero/index.tsx
@@ -10,28 +10,27 @@ export default function Hero() {
         }
     };
     return (
-        <section className="flex flex-col h-full w-full  sm:px-12">
+        <section className="flex flex-col h-[80vh] w-full px-4 sm:px-12">
 
-            <div className="h-[60vh] sm:h-[80vh] w-full flex items-center justify-center">
-                <div className="flex flex-col">
-                    <h1 className="text-5xl text-center md:text-6xl title bg-gradient-to-r from-purple-400 via-pink-400 to-orange-300 bg-clip-text text-transparent">
+            <div className="h-full sm:h-[80vh] w-full flex flex-col items-center justify-center">
+                <div className="flex flex-col text-center">
+                    <h1 className="text-[2.8rem] leading-[3.2rem] md:text-6xl title bg-gradient-to-r from-purple-400 via-pink-400 to-orange-300 bg-clip-text text-transparent">
                         Viagem ao Céu Noturno
                     </h1>
 
-                    <p className="text-sm md:text-lg text-center text-gray-300 opacity-80 pt-2">
-                        A ponte entre o conhecimento ancestral e a exploração do cosmos.
+                    <p className="text-lg text-center text-gray-300 opacity-80 pt-2">
+                        A ponte entre o conhecimento ancestral e a exploração do cosmos
                     </p>
-                    <div className="flex justify-center mt-10">
-                        <a
-                            onClick={scrollToSection}
-                            href="#proxima-sessao"
-                            className="animate-bounce text-white/70 hover:text-white transition cursor-pointer"
-                            aria-label="Role para baixo"
-                        >
-                            <ChevronDown size={40} />
-                        </a>
-                    </div>
+
                 </div>
+                    {/* <a
+                        onClick={scrollToSection}
+                        href="#proxima-sessao"
+                        className=" animate-bounce text-white/70 hover:text-white transition cursor-pointer"
+                        aria-label="Role para baixo"
+                    >
+                        <ChevronDown size={40} />
+                    </a> */}
 
             </div>
 
